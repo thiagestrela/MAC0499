09895236639fb5d4cc2ce45113b73ab1423bf0f9
Thiago Estrela <thiagestrela@gmail.com>
2019-08-31 12:20:09 -0300

    Implemented Gale-Shapley algorithm

--- GaleShapley/test.cpp
+++ GaleShapley/test.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> GaleShapley(vector<vector<int>>&);
+vector<int> GaleShapley(const vector<vector<int>>&, int, int, int);
 
 int main() {
-    int V;
-    scanf("%d", &V);
+    int nA, nB;
+    scanf("%d %d", &nA, &nB);
+    int V = nA + nB;
     vector<vector<int>> prefs(V, vector<int>());
+
     int x;
     for (int i = 0; i < V; i++) {
         do {
@@ -14,24 +16,39 @@ int main() {
             if (x != -1) prefs[i].push_back(x);
         } while (x != -1);
     }
-    vector<int> emp = GaleShapley(prefs);    
+
+    vector<int> Aopt, Bopt;
+    Aopt = GaleShapley(prefs, nA, nB, 0);
+    Bopt = GaleShapley(prefs, nA, nB, 1);
+
     printf("Emparelhamento A-ótimo:\n");
-    for (int i = 0; i < V/2; i++) {
-        if (emp[i] != -1)
-            printf("%d %d\n", i, emp[i]);
-    } 
+    for (int i = 0; i < nA; i++) {
+        if (Aopt[i] != -1)
+            printf("%d %d\n", i, Aopt[i]);
+    }
+    printf("Emparelhamento B-ótimo:\n");
+    for (int i = 0; i < nA; i++) {
+        if (Bopt[i] != -1)
+            printf("%d %d\n", i, Bopt[i]);
+    }
     return 0;
 }
 
-vector<int> GaleShapley(vector<vector<int>> &prefs) {
-   int V = (int)prefs.size();
-   vector<queue<int>> lists(V);
+vector<int> GaleShapley(const vector<vector<int>>& prefs, int nA, int nB, int orientation) {
+   int V = nA + nB;
    vector<int> emp(V, -1);
-   for (int i = 0; i < V; i++) {
-       for (int j : prefs[i]) {
-           lists[i].push(j);
+   vector<queue<int>> lists(V, queue<int>());
+
+   int n = (orientation == 0 ? nA : nB);
+   int offset = (orientation == 0 ? 0 : nB);
+   for (int i = 0; i < n; i++) {
+       int idx = offset + i;
+       for (int j : prefs[idx]) {
+           lists[idx].push(j);
        }
    } 
+   
+   
    vector<vector<int>> Apos(V, vector<int>(V, -1));
    int w;
    for (int i = 0; i < V; i++) {
@@ -42,15 +59,15 @@ vector<int> GaleShapley(vector<vector<int>> &prefs) {
    }
    
    queue<int> free_verts;
-   for (int i = 0; i < V/2; i++) {
-       free_verts.push(i);
+   for (int i = 0; i < n; i++) {
+       int idx = offset + i;
+       free_verts.push(idx);
    }
    
    int a, b;
    while (!free_verts.empty()) {
        a = free_verts.front(); free_verts.pop();
        b = lists[a].front(); lists[a].pop();
-       cout << a << " " << b << endl;
        if (emp[b] == -1) {
            emp[a] = b; emp[b] = a;
        }
